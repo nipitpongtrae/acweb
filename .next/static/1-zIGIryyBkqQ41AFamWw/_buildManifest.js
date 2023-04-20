8e0270d94e66a0c849827373018f37c1404fef3c
Nipitpong Traetulakurn <nipitpongtrae@gmail.com>
2023-04-20 13:52:24 +0700

    First Commit

---
+++ .next/static/1-zIGIryyBkqQ41AFamWw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a6086129adba8575.css","static/chunks/pages/index-fff8368bf1ad4d68.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
